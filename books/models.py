05125a22ad1e4a9c9935d60b79d22565ef459de2
Shawna Cooper <scooper1920@gmail.com>
2022-05-09 18:25:26 -0400

    (completed goal 1)

--- books/models.py
+++ books/models.py
@@ -8,3 +8,11 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Book(models.Model):
+    title       = models.CharField(max_length=100, null=True)
+    author      = models.CharField(max_length=100, null=True)
+    url         = models.URLField(blank=True,null=True)
+    description = models.TextField(null = True, blank = True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    
