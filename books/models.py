8e5c0701af4a2e8635010cfe1ddde20101febaa1
Shawna Cooper <scooper1920@gmail.com>
2022-05-10 11:43:32 -0400

    (started favorites)

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 
 class User(AbstractUser):
@@ -15,4 +17,26 @@ class Book(models.Model):
     url         = models.URLField(blank=True,null=True)
     description = models.TextField(null = True, blank = True)
     created_at  = models.DateTimeField(auto_now_add=True)
+#in this case below a book can have 1 category one to many relationship, like a library
+    category =  models.ForeignKey('Category', related_name='books',null = True, on_delete=models.CASCADE )
     
+
+class  Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(null=False, unique =True)
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs = {'slug': self.slug})
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+#create the slug path in urls from the slug tutorial in your slack
+#here we are overridng the behavior of a model
+#foreign keys need an ondelete
+
+class Favorite(models.Model):
+    book = models.ForeignKey('Book', related_name='favorites',on_delete=models.CASCADE)
+    user = models.ForeignKey('User', related_name= 'favorites', on_delete=models.CASCADE)
